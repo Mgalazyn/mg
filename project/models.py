89228152526e1bedf4c298437a4266298cc38bcc
Mgalazyn <m.galazynn@gmail.com>
2022-12-17 21:22:09 +0100

    build main

--- project/models.py
+++ project/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+class Project(models.Model):
+    name = models.TextField(max_length=250, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, 
+                        primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ['created']
+
+
+class PartLife(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False)
+    short_intro = models.CharField(max_length=250, blank=True, null=True)
+    description = models.TextField(null=True, blank=True)
+    images = models.ImageField(null=True, blank=True, upload_to='', default='')
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, 
+                        primary_key=True, editable=False)
+
+    def __str_(self) -> str:
+        return self.title
+
